af86fbf9758d1d67d4d88686d146f399d2325f6a
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-29 23:10:07 +0600

    getting and setting value from redis
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -6,16 +6,43 @@ import (
 )
 
 func main() {
-	ExampleNewCient()
+	client, err := ExampleNewCient()
+	if err != nil {
+		panic(err)
+	}
+
+	// setting value
+	err = settingValue(client)
+	if err != nil {
+		panic(err)
+	}
+
+	// getting value
+	val, err := gettingValue(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
 }
 
-func ExampleNewCient() {
+func ExampleNewCient() (*redis.Client, error){
 	options := redis.Options{
 		Addr: "localhost:6379",
 		Password: "", // No Password Set
 		DB: 0, // use default database
 	}
 	client := redis.NewClient(&options)
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+	_, err := client.Ping().Result()
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func settingValue(client *redis.Client) error {
+	return client.Set("Key", "value", 0).Err()
+}
+
+func gettingValue(client *redis.Client) (interface{}, error) {
+	return client.Get("Key").Result()
 }
